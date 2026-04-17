1ab0a17668eb1359074e5d4ca4c56871b8a0ae06
Harish Chintal <harishchintal18@gmail.com>
2026-04-18 00:22:31 +0530

    Cleaned project and added files

--- FullStack-E-Learning-Platform/FullStackELearningPlatform/obj/Debug/net8.0/FullStackELearningPlatform.AssemblyInfo.cs
+++ FullStack-E-Learning-Platform/FullStackELearningPlatform/obj/Debug/net8.0/FullStackELearningPlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullStackELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab7d8e898f43ecd027d11bb542d669303789aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullStackELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullStackELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
